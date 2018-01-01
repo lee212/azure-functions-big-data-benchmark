6388c6c7c2bed76cb39b27a8d9b3a3fd60e2d4a3
Hyungro Lee <hroe.lee@gmail.com>
2017-12-31 22:45:56 -0500

    v3

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,10 +1,10 @@
 module.exports = function (context, req) {
-    context.log('[v2] Start of JS through HTTP');
+    context.log('[v3] Start of JS through HTTP');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Version 2 " + (req.query.name || req.body.name)
+            body: "Version 3 " + (req.query.name || req.body.name)
         };
     }
     context.done();
